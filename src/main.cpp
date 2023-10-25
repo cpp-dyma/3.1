021b34976ee691f80c15acb18eb2f6fa4a98bdae
Jérôme Savin <jsavin@jsdev.fr>
2023-10-25 17:08:26 +0200

    Ajout de commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello world!"<<std::endl;
+int main()
+{
+    
+    int abc;                                                    //Instruction de déclaration
+    abc = 5;                                                    //Instruction d'expression
+    abc = 5+1;                                                  //Instruction d'expression
+    ;                                                           //Instruction nulle
+    std::cout << abc << std::endl;                              //Instruction d'expression
 
-    int abc;
-    abc = 5;
-    abc = 5+1;
-    std::cout << abc << std::endl;
-
-    if(abc > 3){
-        std::cout << "abs est plus grand que 3"<<std::endl;
+    
+    if(abc > 3){                                                //Instruction de sélection
+        std::cout << "abc est plus grand que 3"<<std::endl;     //Instruction d'expression
     }
 
-    {
-        int xyz = 4;
-        std::cout << xyz << std::endl;
+    
+    {                                                           //Instruction composée
+        int xyz = 4;                                            //Instruction de déclaration
+        std::cout << xyz << std::endl;                          //Instruction d'expression
     }
+
+    return 0;                                                   //Instruction d'expression
 }
